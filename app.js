b5260e0fdc3822ce05d6c9f45776073d8b80ed33
Bilguun13 <83194126+Bilguun13@users.noreply.github.com>
2021-12-05 01:19:41 +0800

    fully functional calculator

--- app.js
+++ app.js
@@ -18,17 +18,22 @@ function operate (operator, a, b) {
            return 'error'
     }
 }
-let leftValue = 0,
-    rightValue = 0,
+let leftValue,
+    rightValue,
     selectedOperator,
-    secondOperator;
+    secondOperator,
+    canDelete = true;
+    clearDisplay = false;
 
 function inputNumber() {
     const numbers = document.querySelectorAll('.number');
     numbers.forEach((number) => {
         number.addEventListener('click', () => {
             const display = document.querySelector('.input');
-            display.textContent +=number.textContent; 
+            display.textContent +=number.textContent;
+            if(clearDisplay) display.textContent = '' + number.textContent;
+            clearDisplay = false; 
+            canDelete = true;
         })
     })
 }
@@ -42,9 +47,10 @@ function clickOperator () {
         operator.addEventListener('click', () => {
             storeValues();
             storeSecondOperator(operator);
-            display.textContent = '';
+            toggleHighlight(operator);
             calculateValues();
-            test4();
+            blinkDisplay();
+            clearDisplay = true;
         })
     })
 
@@ -55,61 +61,115 @@ function clickEqual () {
 
     equalSign.addEventListener('click', () => {
         storeValues();
-        test3();
+        equalCalculateValues();
+        unhighlightOperators();
     })
 }
 
 function storeValues() {
 
     const display = document.querySelector('.input');
-    if (selectedOperator) {
-        rightValue = parseInt(display.textContent);
-    }else{
-        leftValue = parseInt(display.textContent);  
-        display.classList.add('blink')
+    if (display.textContent){
+        if (selectedOperator) {
+            rightValue = parseInt(display.textContent);
+        }else{
+            leftValue = parseInt(display.textContent);  
+        }
     }
+
 }
 
 function calculateValues() {
     if (leftValue && rightValue) {
         let result;
         result = operate(selectedOperator, leftValue, rightValue);
-        console.log(result)
         leftValue = result;
     }
     if (secondOperator) {
         selectedOperator = secondOperator;
         secondOperator = '';
     }
+    display.textContent = leftValue;
+    canDelete = false;
 }
 
-function test3() {
+function equalCalculateValues() {
     if (leftValue && rightValue) {
         let result;
         result = operate(selectedOperator, leftValue, rightValue);
-        console.log(result)
         leftValue = result;
         selectedOperator = '';
         rightValue = '';
+        display.textContent = leftValue
+        canDelete = false;
     }
 }
 //check if operator was clicked for the second time or not
 function storeSecondOperator(operator) {
-    if (selectedOperator) {
-        secondOperator = operator.textContent;
-    }else{
-        selectedOperator = operator.textContent;
+    if(display.textContent){
+        if (selectedOperator) {
+            secondOperator = operator.textContent;
+        }else{
+            selectedOperator = operator.textContent;
+        }
     }
+
 }
 
 //if left value is there and operator is clicked, next time input entered, display clears
 
-function test4() {
+function blinkDisplay() {
     display.classList.add('blink')
 }
 
+//highlight the chosen operator
 
- const display = document.querySelector('.input');
- display.addEventListener('transitionend', () => {
- display.classList.remove('blink');
+function toggleHighlight(operator){
+    unhighlightOperators();
+    highlightSelectedOperator(operator);
+}
+
+function unhighlightOperators() {
+    const operators = document.querySelectorAll('.operator');
+    operators.forEach((operator) => {
+        operator.classList.remove('highlightChoice')
+    })
+}
+
+function highlightSelectedOperator (operator) {
+    operator.classList.add('highlightChoice')
+}
+
+function clearAll() {
+    leftValue = 0,
+    rightValue = 0,
+    selectedOperator = null,
+    secondOperator = null,
+    clearDisplay = false;
+    display.textContent = ''
+}
+
+function deleteLast() {
+    if (canDelete) {
+    display.textContent = display.textContent.slice(0,-1);
+ }
+}
+
+
+ window.addEventListener('load', () => {
+    inputNumber();
+    clickOperator();
+    clickEqual();
  })
+
+
+const backspace = document.querySelector('.delete')
+backspace.addEventListener('click', deleteLast)
+
+const AC = document.querySelector('.ac')
+AC.addEventListener('click', clearAll )
+
+const display = document.querySelector('.input');
+display.addEventListener('transitionend', () => {
+    display.classList.remove('blink');
+})
